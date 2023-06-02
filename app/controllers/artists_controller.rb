0b7a2b19d2ab32419acb039a81dfa8b325b1781b
Gianluca Galli <gianluca.galli9519@gmail.com>
2023-06-02 14:04:12 +0100

    artist view

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,20 @@
 class ArtistsController < ApplicationController
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.image = params[:artist][:image]
+
+    if @artist.save
+
+    else
+
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
+
 end
